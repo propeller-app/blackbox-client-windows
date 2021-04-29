b745622cb07056e149ade6bbcd8b07a78cb02426
Ben <ben@solero.me>
2021-04-29 23:38:07 +0100

    feat: job queue form skeleton

---
+++ client/redhvid-client/JobQueueForm.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace Redhvid
+{
+    partial class JobQueueForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.jobsDataGrid = new System.Windows.Forms.DataGridView();
+            this.JobNo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.JobStatus = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.jobsDataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Job Queue";
+            // 
+            // jobsDataGrid
+            // 
+            this.jobsDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.jobsDataGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.JobNo,
+            this.JobStatus});
+            this.jobsDataGrid.Location = new System.Drawing.Point(12, 53);
+            this.jobsDataGrid.Name = "jobsDataGrid";
+            this.jobsDataGrid.RowTemplate.Height = 25;
+            this.jobsDataGrid.Size = new System.Drawing.Size(360, 496);
+            this.jobsDataGrid.TabIndex = 2;
+            // 
+            // JobNo
+            // 
+            this.JobNo.HeaderText = "Job Number";
+            this.JobNo.Name = "JobNo";
+            // 
+            // JobStatus
+            // 
+            this.JobStatus.HeaderText = "Job Status";
+            this.JobStatus.Name = "JobStatus";
+            this.JobStatus.ReadOnly = true;
+            // 
+            // JobQueueForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 561);
+            this.Controls.Add(this.jobsDataGrid);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "JobQueueForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Jobs";
+            ((System.ComponentModel.ISupportInitialize)(this.jobsDataGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView jobsDataGrid;
+        private System.Windows.Forms.DataGridViewTextBoxColumn JobNo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn JobStatus;
+    }
+}

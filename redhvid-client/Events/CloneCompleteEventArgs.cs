f160207de84f54fe93b67b13ca295f1cccb6fb16
Ben <ben.pearce@sky.com>
2021-06-18 14:40:50 +0100

    feat: clone progress and complete event args classes

---
+++ redhvid-client/Events/CloneCompleteEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Redhvid.Events
+{
+    public class CloneCompleteEventArgs : EventArgs
+    {
+        public CloneCompleteEventArgs(int fileTotal, DirectoryInfo outputDir)
+        {
+            FileTotal = fileTotal;
+            OutputDir = outputDir;
+        }
+
+        public int FileTotal { get; set; }
+        public DirectoryInfo OutputDir { get; set; }
+    }
+}
---
+++ redhvid-client/Events/CloneProgressEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Redhvid.Events
+{
+    public class CloneProgressEventArgs : EventArgs
+    {
+        public CloneProgressEventArgs(int fileNum, string fileName, int fileTotal, decimal percent)
+        {
+            FileNum = fileNum;
+            FileName = fileName;
+            FileTotal = fileTotal;
+            Percent = percent;
+        }
+
+        public int FileNum { get; set; }
+        public string FileName { get; set; }
+        public int FileTotal { get; set; }
+        public decimal Percent { get; set; }
+    }
+}

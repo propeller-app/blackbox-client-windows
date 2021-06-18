eb8d2abaa28a987af06fdfba7b200dd0c9b2cd79
Ben <ben.pearce@sky.com>
2021-06-18 14:56:14 +0100

    feat: update utils.cs

---
+++ redhvid-client/Utils.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Redhvid
+{
+    public static class Utils
+    {
+        public static String GetGRPCUrl()
+        {
+            String proto = Properties.Settings.Default.GrpcSSL ? "https" : "http";
+            String host = Properties.Settings.Default.GrpcHost;
+            uint port = Properties.Settings.Default.GrpcPort;
+            return $"{proto}://{host}:{port}";
+        }
+
+        public static DirectoryInfo GetTempJobDirectory(Job job)
+        {
+            return Directory.CreateDirectory(Path.Combine(
+                Path.GetTempPath(),
+                "redh",
+                $"job_{job.number}"
+            ));
+        }
+
+        public static Point GetTrayWindowLocation(Form form)
+        {
+            Rectangle workingArea = Screen.GetWorkingArea(form);
+            return new Point(workingArea.Width - form.Width,  workingArea.Height - form.Height);
+        }
+    }
+}

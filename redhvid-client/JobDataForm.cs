c87e03a09e01a9c0d31c8af5822248ccd48197eb
Ben <ben.pearce@sky.com>
2021-06-18 14:42:28 +0100

    feat: Job data input form class

---
+++ redhvid-client/JobDataForm.cs
@@ -0,0 +1,50 @@
+﻿using Redhvid.Events;
+using System;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Redhvid
+{
+    public partial class JobDataForm : Form
+    {
+
+        private readonly Job job;
+        public JobDataForm(Job job)
+        {
+            InitializeComponent();
+            Location = Utils.GetTrayWindowLocation(this);
+
+            this.job = job;
+            job.CloneProgress += CloneProgress;
+            job.CloneComplete += CloneComplete;
+        }
+
+        private void CloneProgress(object sender, CloneProgressEventArgs e)
+        {
+            Invoke(new Action(() =>
+            {
+                statusProgressBar.Value = (int)Math.Round(e.Percent * 100m);
+                statusLabel.Text = $"Cloning ({e.FileNum}/{e.FileTotal})";
+            }));
+        }
+
+        private void CloneComplete(object sender, CloneCompleteEventArgs e)
+        {
+            Invoke(new Action(() =>
+            {
+                statusProgressBar.Value = 0;
+                statusProgressBar.Enabled = false;
+                statusLabel.Text = "Ready";
+            }));
+        }
+
+        private void uploadButton_Click(object sender, EventArgs e)
+        {
+            job.SetJobData(new JobData(firstNameTextBox.Text, lastNameTextBox.Text, emailTextBox.Text));
+
+            Close();
+            Program.ShowJobProgress();
+        }
+    }
+}

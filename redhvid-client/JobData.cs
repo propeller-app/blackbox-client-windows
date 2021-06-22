108c95c3dd5a9f661612a114aff46d4a5902370c
Ben <ben.pearce@sky.com>
2021-06-22 23:36:33 +0100

    feat: `JobData` class to hold customer form data

---
+++ redhvid-client/JobData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Redhvid
+{
+    public class JobData
+    {
+        public JobData(string customerFirstName, string customerLastName, string customerEmail)
+        {
+            CustomerFirstName = customerFirstName;
+            CustomerLastName = customerLastName;
+            CustomerEmail = customerEmail;
+        }
+
+        public string CustomerFirstName { get; set; }
+        public string CustomerLastName { get; set; }
+        public string CustomerEmail { get; set; }
+    }
+}

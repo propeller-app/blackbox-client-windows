99eac63c82efebdb2f3fc03c253499f3ce79454c
Ben <ben.pearce@sky.com>
2021-06-22 23:29:51 +0100

    feat(events): job cancelled event arguments

---
+++ redhvid-client/Events/JobCancelledEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Redhvid.Events
+{
+    public class JobCancelledEventArgs : EventArgs
+    {
+        public JobCancelledEventArgs(Exception reason)
+        {
+            Reason = reason;
+        }
+
+        public Exception Reason { get; set; }
+    }
+}

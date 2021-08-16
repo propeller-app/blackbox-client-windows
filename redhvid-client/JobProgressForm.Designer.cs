97d20fa2637ee05330f2ed3a88bf20047b42c4b4
Ben <ben.pearce@sky.com>
2021-08-16 12:10:38 +0100

    feat: add job listeners to job progress form

--- redhvid-client/JobProgressForm.Designer.cs
+++ redhvid-client/JobProgressForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿
+
 namespace Redhvid
 {
     partial class JobProgressForm
@@ -121,21 +121,21 @@ namespace Redhvid
             this.closeButton.UseVisualStyleBackColor = true;
             this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
             // 
-            // progressBar1
+            // jobProgress
             // 
-            this.progressBar1.Location = new System.Drawing.Point(12, 37);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(334, 44);
-            this.progressBar1.TabIndex = 2;
+            this.jobProgress.Location = new System.Drawing.Point(12, 37);
+            this.jobProgress.Name = "jobProgress";
+            this.jobProgress.Size = new System.Drawing.Size(334, 44);
+            this.jobProgress.TabIndex = 2;
             // 
-            // label1
+            // progressLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(39, 15);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Ready";
+            this.progressLabel.AutoSize = true;
+            this.progressLabel.Location = new System.Drawing.Point(12, 9);
+            this.progressLabel.Name = "progressLabel";
+            this.progressLabel.Size = new System.Drawing.Size(39, 15);
+            this.progressLabel.TabIndex = 3;
+            this.progressLabel.Text = "Ready";
             // 
             // JobProgressForm
             // 
@@ -143,8 +143,8 @@ namespace Redhvid
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(358, 137);
             this.ControlBox = false;
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.progressLabel);
+            this.Controls.Add(this.jobProgress);
             this.Controls.Add(this.closeButton);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -170,7 +170,7 @@ namespace Redhvid
         public System.Windows.Forms.NotifyIcon trayIcon;
         private System.Windows.Forms.Button closeButton;
         private System.Windows.Forms.ToolStripMenuItem settingsMenuItem;
-        private System.Windows.Forms.ProgressBar progressBar1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar jobProgress;
+        private System.Windows.Forms.Label progressLabel;
     }
 }
--- redhvid-client/JobProgressForm.cs
+++ redhvid-client/JobProgressForm.cs
@@ -1,11 +1,16 @@
-﻿using System;
+using Redhvid.Events;
+using System;
+using System.Diagnostics;
 using System.Drawing;
 using System.Windows.Forms;
+using Xabe.FFmpeg.Events;
 
 namespace Redhvid
 {
     public partial class JobProgressForm : Form
     {
+        Job job;
+
         public JobProgressForm()
         {
             InitializeComponent();
@@ -18,6 +23,44 @@ namespace Redhvid
                 workingArea.Width - Width,
                 workingArea.Height - Height
             );
+        }
+        public void SetJob(Job job)
+        {
+            this.job = job;
+            job.CloneProgress += CloneProgress;
+            job.TranscodeProgress += TranscodeProgress;
+            job.JobComplete += JobComplete;
+        }
+
+        public void CloneProgress(object sender, CloneProgressEventArgs e)
+        {
+            Invoke(new Action(() =>
+            {
+                jobProgress.Value = (int)Math.Round((((e.FileNum - 1) + e.Percent) / e.FileTotal)*50);
+                progressLabel.Text = $"Cloning ({e.FileNum}/{e.FileTotal})";
+            }));
+        }
+
+        public void TranscodeProgress(object sender, ConversionProgressEventArgs e)
+        {
+            Invoke(new Action(() =>
+            {
+                jobProgress.Value = (jobProgress.Value + 10) % 100;
+                progressLabel.Text = $"Transcoding {e.Duration}";
+            }));
+        }
+
+        public void JobComplete(object sender, JobCompleteEventArgs e)
+        {
+            Invoke(new Action(() =>
+            {
+                jobProgress.Value = 0;
+                progressLabel.Text = "Ready";
+            }));
+
+            job.CloneProgress -= CloneProgress;
+            job.TranscodeProgress -= TranscodeProgress;
+            job.JobComplete -= JobComplete;
         }
 
         private void trayIcon_MouseDoubleClick(object sender, MouseEventArgs e)

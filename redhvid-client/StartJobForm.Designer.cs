2194bd51d7e3dbea0da70c3169ec9af6ea722b4d
Ben <ben.pearce@sky.com>
2021-08-16 12:26:38 +0100

    feat: start job form first implementation

---
+++ redhvid-client/StartJobForm.Designer.cs
@@ -0,0 +1,81 @@
+﻿
+namespace Redhvid
+{
+    partial class StartJobForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.beginJobButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // beginJobButton
+            // 
+            this.beginJobButton.Location = new System.Drawing.Point(12, 12);
+            this.beginJobButton.Name = "beginJobButton";
+            this.beginJobButton.Size = new System.Drawing.Size(281, 41);
+            this.beginJobButton.TabIndex = 0;
+            this.beginJobButton.Text = "Begin Job";
+            this.beginJobButton.UseVisualStyleBackColor = true;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(12, 59);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(281, 41);
+            this.cancelButton.TabIndex = 1;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // StartJobForm
+            // 
+            this.AcceptButton = this.beginJobButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.cancelButton;
+            this.ClientSize = new System.Drawing.Size(305, 111);
+            this.ControlBox = false;
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.beginJobButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "StartJobForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Begin Job?";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Button beginJobButton;
+        public System.Windows.Forms.Button cancelButton;
+    }
+}
---
+++ redhvid-client/StartJobForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Redhvid
+{
+    public partial class StartJobForm : Form
+    {
+        public StartJobForm()
+        {
+            InitializeComponent();
+
+            Rectangle workingArea = Screen.GetWorkingArea(this);
+            this.Location = new Point(
+                workingArea.Width - this.Width,
+                workingArea.Height - this.Height
+            );
+        }
+    }
+}

e0017f7ac31054083f7aa7164322844e1e4f6e91
Ben <ben.pearce@sky.com>
2022-02-24 20:39:51 +0000

    refactor: update settings properties

---
+++ Blackbox/Device.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Blackbox
+{
+    [Serializable]
+    public class Device : IEquatable<Device>
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        public Device() { }
+
+        public Device(string id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public bool Equals(Device d) => d.Id == this.Id;
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Device);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+    }
+}
